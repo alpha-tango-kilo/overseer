9d1b0d0c0f36ce577263bb7021b37338da69ec38
alpha-tango-kilo <git@heyatk.com>
2022-06-03 14:44:35 +0100

    Add example FileEventTask config and test app

---
+++ overseer_test/src/main.rs
@@ -0,0 +1,27 @@
+use std::sync::Arc;
+use task::FileEventTask;
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+
+#[tokio::main]
+async fn main() -> color_eyre::Result<()> {
+    tracing::subscriber::set_global_default(
+        FmtSubscriber::builder()
+            .with_max_level(Level::TRACE)
+            .with_thread_ids(true)
+            .finish(),
+    )
+    .unwrap();
+    color_eyre::install().unwrap();
+
+    info!("Loading from file");
+    let file_task =
+        FileEventTask::load_from("task/examples/file_task.yml").await?;
+    eprintln!("{file_task:#?}");
+    let file_task = Arc::new(file_task);
+    info!("Activating");
+    file_task.clone().activate().await?.await?;
+    info!("Job done");
+    std::mem::drop(file_task);
+    Ok(())
+}
