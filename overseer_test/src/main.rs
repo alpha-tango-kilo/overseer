e8fe7dfc7c459dc38fac14c647c514482194f8b9
alpha-tango-kilo <git@heyatk.com>
2022-06-15 19:31:38 +0100

    Use overseer_test to test service functionality
    It works!

--- overseer_test/src/main.rs
+++ overseer_test/src/main.rs
@@ -1,7 +1,16 @@
+#![allow(unused_imports, dead_code)]
+
+use service::docker::DockerContainer;
+use service::Service;
 use std::sync::Arc;
 use task::FileEventTask;
 use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
+
+const SERVICE_STR: &str = r"
+name: jellyfin
+host: localhost
+";
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
@@ -14,13 +23,20 @@ async fn main() -> color_eyre::Result<()> {
     .unwrap();
     color_eyre::install().unwrap();
 
-    info!("Loading from file");
+    let mut jellyfin = serde_yaml::from_str::<DockerContainer>(SERVICE_STR)?;
+    jellyfin.connect().await?;
+    info!(?jellyfin);
+    let jellyfin = Arc::new(jellyfin);
+    let status = (&jellyfin).status().await?;
+    info!(%status);
+
+    /*info!("Loading from file");
     let file_task =
         FileEventTask::load_from("task/examples/file_task.yml").await?;
     eprintln!("{file_task:#?}");
     let file_task = Arc::new(file_task);
     info!("Activating");
     file_task.activate().await?.await?;
-    info!("Job done");
+    info!("Job done");*/
     Ok(())
 }
